c8ea938056be5fad737b202d93269ac60a162f3f
rahul-shahtp <rahulshah6611@gmail.com>
2026-07-12 14:25:59 +0530

    fix minor issue IF stage

--- rtl/if_stage/pc_adder.v
+++ rtl/if_stage/pc_adder.v
@@ -6,11 +6,3 @@ module pc_adder (
         pc_plus4_o = pc_n + 4;
     end
 endmodule
-module pc_adder (
-    input      [31:0] pc_n,
-    output reg [31:0] pc_plus4_o
-);
-    always @(*) begin
-        pc_plus4_o = pc_n + 4;
-    end
-endmodule
