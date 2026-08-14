5a5ffd1ec81d42702a73c08e4c3f394b8f93864b
rahul-shahtp <rahulshah6611@gmail.com>
2026-08-14 11:30:28 +0530

    Adding rom contents for synthesis

--- rtl/if_stage/instr_mem.v
+++ rtl/if_stage/instr_mem.v
@@ -5,6 +5,11 @@ module instr_mem (
     localparam MAX_SIZE = 1024 ;
     reg [31:0] memory [0:MAX_SIZE-1];
 
+`ifdef SYNTHESIS
+    // ROM contents for synthesis
+    initial $readmemh("hex/test_fibonacci.hex", memory);
+`endif
+
     assign instruction_o = memory[address_n[11:2]];
 
 endmodule
